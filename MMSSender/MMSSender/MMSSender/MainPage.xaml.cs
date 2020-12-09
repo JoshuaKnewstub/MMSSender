1948cf7372d1c0462b8abd3a07729fb518942a29
Joshua Knewstub <36719680+JoshuaKnewstub@users.noreply.github.com>
2020-12-10 08:56:33 +1000

    add layout and share functions

--- MMSSender/MMSSender/MMSSender/MainPage.xaml.cs
+++ MMSSender/MMSSender/MMSSender/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.IO;
 
 namespace MMSSender
 {
@@ -13,6 +15,34 @@ namespace MMSSender
         public MainPage()
         {
             InitializeComponent();
+
         }
+
+        private async void ButtonShare_Clicked(object sender, EventArgs e)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = EntryShare.Text,
+                Title = "Share"
+            });
+        }
+
+        private async void ShareUri(object sender, EventArgs e)
+        {
+            string url = "https://prolist.net.au/WebServices/AgentFileService.asmx/PublicDownload?id=30fe9a79-ea96-46a7-8399-7737bd0bf323&inline=true";
+
+            string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "imagedata.tmp");
+
+
+
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                Title = Title,
+                //This crashes
+                File = new ShareFile("https://prolist.net.au/WebServices/AgentFileService.asmx/PublicDownload?id=30fe9a79-ea96-46a7-8399-7737bd0bf323&inline=true")
+            });
+        }
+
+
     }
 }
