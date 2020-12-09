dc8445de5e1ede06f7986d9fa816782a544b626d
JamesRaymer <james.raymer@my.jcu.edu.au>
2020-12-10 08:53:53 +1000

    Add fileShare method

--- MMSSender/MMSSender/MMSSender/MainPage.xaml.cs
+++ MMSSender/MMSSender/MMSSender/MainPage.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
 
 namespace MMSSender
 {
@@ -13,6 +17,52 @@ namespace MMSSender
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void ButtonShare_Clicked(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            Image img = new Image();
+            System.IO.Stream stream = await client.GetStreamAsync("https://prolist.net.au/WebServices/AgentFileService.asmx/PublicDownload?id=30fe9a79-ea96-46a7-8399-7737bd0bf323&inline=true");
+            img.Source = ImageSource.FromStream(() => stream);
+            string tempFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "imagedata.tmp");
+            var fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
+
+            using (fileStream)
+                {
+                stream.CopyTo(fileStream);
+                }
+
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = EntryShare.Text,
+                Title = "Share!"
+            }) ;
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                File = new ShareFile(tempFile)
+            }); ;
+
+        }
+
+        private async void ImageShare_Clicked(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            Image img = new Image();
+            System.IO.Stream stream = await client.GetStreamAsync("https://prolist.net.au/WebServices/AgentFileService.asmx/PublicDownload?id=30fe9a79-ea96-46a7-8399-7737bd0bf323&inline=true");
+            img.Source = ImageSource.FromStream(() => stream);
+            string tempFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "imagedata.tmp");
+            var fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
+
+            using (fileStream)
+            {
+                stream.CopyTo(fileStream);
+            }
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                File = new ShareFile(tempFile)
+            }); ;
+
         }
     }
 }
