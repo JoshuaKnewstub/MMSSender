76b37a1e82b3e5afe098a9022d1fd104c9979a98
Joshua Knewstub <36719680+JoshuaKnewstub@users.noreply.github.com>
2020-12-10 10:02:01 +1000

    Format and cleaning

--- MMSSender/MMSSender/MMSSender/MainPage.xaml.cs
+++ MMSSender/MMSSender/MMSSender/MainPage.xaml.cs
@@ -16,29 +16,11 @@ namespace MMSSender
 
         private async void ButtonShare_Clicked(object sender, EventArgs e)
         {
-            HttpClient client = new HttpClient();
-            Image img = new Image();
-            System.IO.Stream stream = await client.GetStreamAsync("https://prolist.net.au/WebServices/AgentFileService.asmx/PublicDownload?id=30fe9a79-ea96-46a7-8399-7737bd0bf323&inline=true");
-            img.Source = ImageSource.FromStream(() => stream);
-            string tempFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "imagedata.tmp");
-            var fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
-
-            using (fileStream)
-                {
-                stream.CopyTo(fileStream);
-                }
-
             await Share.RequestAsync(new ShareTextRequest
             {
                 Text = EntryShare.Text,
                 Title = "Share!"
             }) ;
-
-            await Share.RequestAsync(new ShareFileRequest
-            {
-                File = new ShareFile(tempFile)
-            }); ;
-
         }
 
         private async void ImageShare_Clicked(object sender, EventArgs e)
